07718df7754117d1e3b20fea6c209d40d93814f3
Vincent G <vincent.godard.45@gmail.com>
2022-07-18 16:16:14 +0200

    Local work is done

--- P7_Scoring/Features_extractions.py
+++ P7_Scoring/Features_extractions.py
@@ -2,9 +2,14 @@ import pandas as pd
 
 
 def get_my_df() -> object:
-    path = 'C:/Users/33646/Documents/OpenClassroom/Projet 7/Model_of_scoring/Datas/data_clients.csv'
-    path = 'Datas/data_clients.csv'
-    df = pd.read_csv(path)
+    try:
+        path = 'C:/Users/33646/Documents/OpenClassroom/Projet 7/Model_of_scoring/Datas/data_clients.csv'
+        df = pd.read_csv(path)
+    except:
+        path = 'Datas/data_clients.csv'
+        df = pd.read_csv(path)
+
+
     df_drop = df.drop(['SK_ID_CURR', 'TARGET'], axis=1)
     cols = pd.DataFrame(df_drop.columns, columns=['Features'])
 
--- P7_Scoring/Model_extraction.py
+++ P7_Scoring/Model_extraction.py
@@ -1,6 +1,5 @@
 import pickle
-import dill
-from lime import lime_tabular
+
 
 
 def get_my_model() -> object:
@@ -9,15 +8,31 @@ def get_my_model() -> object:
     :rtype: object
     """
     # Charger le best model
-    with open('C:/Users/33646/Documents/OpenClassroom/Projet 7/Model_of_scoring/Datas/best_model', 'rb') as f1:
-        my_model = pickle.load(f1)
+    # with open('C:/Users/33646/Documents/OpenClassroom/Projet 7/Model_of_scoring/Datas/best_model', 'rb') as f1:
+    # my_model = pickle.load(f1)
+    # return my_model
+
+    try:
+        with open('C:/Users/33646/Documents/OpenClassroom/Projet 7/Model_of_scoring/Datas/best_model', 'rb') as f1:
+            my_model = pickle.load(f1)
+    except:
+        with open('Datas/best_model', 'rb') as f1:
+            my_model = pickle.load(f1)
     return my_model
 
 
 def get_my_explainer(data_clients_std, cols):
     # Charger l'explainer'
 
-    with open('C:/Users/33646/Documents/OpenClassroom/Projet 7/Model_of_scoring/Datas/explainer', 'rb') as f:
-        explainer = dill.load(f, errors="ignore")
+    # with open('C:/Users/33646/Documents/OpenClassroom/Projet 7/Model_of_scoring/Datas/explainer', 'rb') as f:
+    # explainer = dill.load(f, errors="ignore")
 
+    # return explainer
+
+    try:
+        with open('C:/Users/33646/Documents/OpenClassroom/Projet 7/Model_of_scoring/Datas/explainer', 'rb') as f:
+            explainer = pickle.load(f, errors="ignore")
+    except:
+        with open('Datas/explainer', 'rb') as f:
+            explainer = pickle.load(f, errors="ignore")
     return explainer
