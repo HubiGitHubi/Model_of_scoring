32d8f93b68ff7376a7a28f50a4b836e44b128896
Vincent G <vincent.godard.45@gmail.com>
2022-07-27 15:31:38 +0200

    Local work is done

--- Streamlit_Fonctions.py
+++ Streamlit_Fonctions.py
@@ -22,11 +22,12 @@ def choose_id_client(df_to_predict):
     return id_client
 
 
-def Calculate_all_score(df_to_predict, df_drop, model):
+def Calculate_all_scores(df_to_predict, df_drop, model):
     # Calculate score for every client and store it in df
-    #print(df_drop.shape, df_to_predict.drop(['SK_ID_CURR'], axis=1).shape)
+    print(df_drop.shape, df_to_predict.drop(['SK_ID_CURR'], axis=1).shape)
     data_clients_std = pd.DataFrame(StandardScaler().fit(df_drop).transform(df_to_predict.drop(['SK_ID_CURR'], axis=1)),
                                     columns=df_drop.columns)
+    print(data_clients_std)
     df_to_predict['score'] = model.predict(data_clients_std.values)
     return data_clients_std
 
@@ -121,7 +122,7 @@ def main():
     df, df_drop, cols, df_to_predict = get_train_test()
     model = get_my_model()
     id_client = choose_id_client(df_to_predict)
-    data_clients_std = Calculate_all_score(df_to_predict, df_drop, model)
+    data_clients_std = Calculate_all_scores(df_to_predict, df_drop, model)
     data_client = calculate_data_client(id_client, df_to_predict, data_clients_std)
     score = calculate_score_id_client(id_client, df_to_predict, data_client)
     score_to_score_str(score)
