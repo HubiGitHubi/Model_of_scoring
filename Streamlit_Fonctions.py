e9657f17e264c8b8cfa9c017e07a2d0530ec36ad
Vincent G <vincent.godard.45@gmail.com>
2022-07-29 10:21:00 +0200

    Local work is done

--- Streamlit_Fonctions.py
+++ Streamlit_Fonctions.py
@@ -1,13 +1,12 @@
-# import _json
 import math
-
 import dill
 import numpy as np
 import streamlit as st
-import streamlit.components.v1 as components
 from matplotlib import pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from P7_Scoring.Extraction import *
+import pickle
+import pandas as pd
 
 
 # Local URL: http: // localhost: 8501
@@ -26,6 +25,56 @@ def add_side_bar(df_to_predict):
     nb_feats = st.sidebar.slider(
         "How many local features do you want ?", 2, 15, step=1)
     return id_client, yes_no_feat_glob, nb_feats
+
+
+def get_my_model() -> object:
+    """
+
+    :rtype: object
+    """
+    # Charger le best model
+    # with open('C:/Users/33646/Documents/OpenClassroom/Projet 7/Model_of_scoring/Datas/best_model', 'rb') as f1:
+    # my_model = pickle.load(f1)
+    # return my_model
+
+    try:
+        with open('C:/Users/33646/Documents/OpenClassroom/Projet 7/Model_of_scoring/Datas/best_model', 'rb') as f1:
+            my_model = pickle.load(f1)
+    except:
+        with open('Datas/best_model', 'rb') as f1:
+            my_model = pickle.load(f1)
+    return my_model
+
+
+def get_my_explainer():
+    # Charge the explainer'
+    try:
+        with open('C:/Users/33646/Documents/OpenClassroom/Projet 7/Model_of_scoring/Datas/explainer', 'rb') as f:
+            explainer = pickle.load(f, errors="ignore")
+    except:
+        with open('Datas/explainer', 'rb') as f:
+            explainer = pickle.load(f, errors="ignore")
+    return explainer
+
+
+def get_train_test() -> object:
+    try:
+        path = 'C:/Users/33646/Documents/OpenClassroom/Projet 7/Model_of_scoring/Datas/data_clients.csv'
+        df = pd.read_csv(path)
+    except:
+        path = 'Datas/data_clients.csv'
+        df = pd.read_csv(path)
+    try:
+        path = 'C:/Users/33646/Documents/OpenClassroom/Projet 7/Model_of_scoring/Datas/data_clients_to_predict.csv'
+        df_to_predict = pd.read_csv(path)
+    except:
+        path = 'Datas/data_clients_to_predict.csv'
+        df_to_predict = pd.read_csv(path)
+
+    df_drop = df.drop(['SK_ID_CURR', 'TARGET'], axis=1)
+    cols = pd.DataFrame(df_drop.columns, columns=['Features'])
+
+    return df, df_drop, cols, df_to_predict
 
 
 def Calculate_all_scores(df_to_predict, df_drop, model):
