4bded38dbfecab5f1812f3984c79ca36b3174fe3
Vincent G <vincent.godard.45@gmail.com>
2022-08-03 10:44:44 +0200

    Local work is done

--- P7_Scoring/Streamlit_Fonctions.py
+++ P7_Scoring/Streamlit_Fonctions.py
@@ -69,7 +69,7 @@ def get_train_test() -> object:
         #path = 'C:/Users/33646/Documents/OpenClassroom/Projet 7/Model_of_scoring/Datas/data_clients.csv'
        # df = pd.read_csv(path)
     #try:
-    path = '/Datas/data_clients_to_predict.csv'
+    path = 'Datas/data_clients_to_predict.csv'
     df_to_predict = pd.read_csv(path)
     #except:
      #   path = 'C:/Users/33646/Documents/OpenClassroom/Projet 7/Model_of_scoring/Datas/data_clients_to_predict.csv'
