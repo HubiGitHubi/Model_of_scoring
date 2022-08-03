badf7ab2733f1e4b6f836244f0024fb34bba2afd
Vincent G <vincent.godard.45@gmail.com>
2022-08-03 14:11:57 +0200

    Local work is done

--- P7_Scoring/Streamlit_Fonctions.py
+++ P7_Scoring/Streamlit_Fonctions.py
@@ -163,7 +163,10 @@ def plot_feat_importance_values(df_feat_importance):
     # Plot the global features importance
     st.write("Global feature importance")
     fig = plt.figure(figsize=(15, 25))
+    st.write("df_feat_importance")
+    st.write(df_feat_importance)
     df_feat_importance_abs = abs(df_feat_importance).reset_index().sort_values(by='Features', ascending=False)#[0:15].index
+    st.write("df_feat_importance_abs")
     st.write(df_feat_importance_abs)
     st.write(df_feat_importance[df_feat_importance_abs.reset_index()].reset_index())
     sns.barplot(data=df_feat_importance[df_feat_importance_abs].reset_index(), x="feat_importance", y='Features')
