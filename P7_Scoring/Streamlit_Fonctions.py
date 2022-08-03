1306c7c9b004fa58512513cdf5ad013dc4456c1e
Vincent G <vincent.godard.45@gmail.com>
2022-08-03 10:37:24 +0200

    Local work is done

--- P7_Scoring/Streamlit_Fonctions.py
+++ P7_Scoring/Streamlit_Fonctions.py
@@ -66,13 +66,13 @@ def get_train_test() -> object:
         path = '/Datas/data_clients.csv'
         df = pd.read_csv(path)
     except:
-        path = '../Datas/data_clients.csv'
+        path = 'C:/Users/33646/Documents/OpenClassroom/Projet 7/Model_of_scoring/Datas/data_clients.csv'
         df = pd.read_csv(path)
     try:
         path = '/Datas/data_clients_to_predict.csv'
         df_to_predict = pd.read_csv(path)
     except:
-        path = '../Datas/data_clients_to_predict.csv'
+        path = 'C:/Users/33646/Documents/OpenClassroom/Projet 7/Model_of_scoring/Datas/data_clients_to_predict.csv'
         df_to_predict = pd.read_csv(path)
 
     df_drop = df.drop(['SK_ID_CURR', 'TARGET'], axis=1)
