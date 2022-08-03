31a1c85bdf35b1f4a442b2b965031557ea01683f
Vincent G <vincent.godard.45@gmail.com>
2022-08-03 10:31:36 +0200

    Local work is done

--- P7_Scoring/dashboard.py
+++ P7_Scoring/dashboard.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 # main function
 def main():
     # URL = "http://192.168.1.27:8501/app/"
-    # URL = "http://192.168.1.22:8501/app/"
+    # URL = "http://127.0.0.1:5000/app/"
     URL = "http://127.0.0.1:5000/app/"
     # Display the title
     st.title('Loan application scoring dashboard')
