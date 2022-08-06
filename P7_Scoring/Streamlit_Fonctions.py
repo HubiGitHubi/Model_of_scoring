4f0a83808302d7c2eeb667a2af180da8d1f21cb5
Vincent G <vincent.godard.45@gmail.com>
2022-08-06 09:15:29 +0200

    Local work is done

--- P7_Scoring/Streamlit_Fonctions.py
+++ P7_Scoring/Streamlit_Fonctions.py
@@ -246,7 +246,7 @@ def Calculate_neighbourhood( df, df_to_predict, nb_neighbours, final_list ):
 
     index_neighbors = neighbors.kneighbors(X=df_to_predict.drop(['SK_ID_CURR', 'score'], axis=1),
                                            n_neighbors=nb_neighbours).ravel()
-    st.write(index_neighbors.shape)
+    st.write(index_neighbors)
 
     neighbors = df.loc[index_neighbors, final_list]
     st.write(neighbors)
