099f106690c15257a993030485fbd43c4befea2c
Vincent G <vincent.godard.45@gmail.com>
2022-08-12 16:38:07 +0200

    Local work is done

--- app.py
+++ app.py
@@ -289,5 +289,5 @@ def find_loc_feat_importance(explanation_list, df_to_predict):
     return jsonify(final_list_json)
 
 
-# if __name__ == main:
-app.run()
+if __name__ == "__main__" :
+    app.run()
