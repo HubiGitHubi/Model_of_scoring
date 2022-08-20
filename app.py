43f510fc0062b60a50c51eb8577bbb5571614d53
Vincent G <vincent.godard.45@gmail.com>
2022-08-20 17:02:33 +0200

    Local work is done

--- app.py
+++ app.py
@@ -47,8 +47,8 @@ def Calculate_all_scores(df_to_predict, model):
 
     x_test_predict = pd.DataFrame(model.predict_proba(data_clients_std.values), columns=['negative', 'positive'])
     x_test_predict['pred'] = -1
-    x_test_predict['pred'][x_test_predict['negative'] >= .6] = 1
-    x_test_predict['pred'][x_test_predict['negative'] < .6] = 0
+    x_test_predict['pred'][x_test_predict['negative'] >= .6] = 0
+    x_test_predict['pred'][x_test_predict['negative'] < .6] = 1
     df_to_predict['score'] = x_test_predict['pred'].values
 
     return data_clients_std
