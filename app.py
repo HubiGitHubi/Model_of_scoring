1beb32d87304d6c30979925869a4ac92f367b54d
Vincent G <vincent.godard.45@gmail.com>
2022-08-20 15:31:45 +0200

    Local work is done

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def Calculate_all_scores(df_to_predict, model):
     x_test_predict['pred'][x_test_predict['negative'] >= .6] = 1
     x_test_predict['pred'][x_test_predict['negative'] < .6] = 0
     df_to_predict['score'] = x_test_predict['pred'].values
-    df_to_predict['score'] = model.predict_proba(data_clients_std.values)
+    #df_to_predict['score'] = model.predict_proba(data_clients_std.values)
 
     return data_clients_std
 
